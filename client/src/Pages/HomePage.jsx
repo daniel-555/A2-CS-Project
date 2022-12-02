b0a5efd043f9464688fe8860159dd295516158b7
Daniel Spiers <danjspiers@gmail.com>
2022-12-02 13:35:06 +0000

    HomePage frontend added

--- client/src/Pages/HomePage.jsx
+++ client/src/Pages/HomePage.jsx
@@ -1,7 +1,26 @@
-import { Title } from "@mantine/core";
+import { Button, Card, SimpleGrid, Title } from "@mantine/core";
 
 const HomePage = () => {
-	return <Title order={1}>This is the Home Page</Title>;
+	return (
+		<Card className="card center">
+			<Title order={1}>Welcome to &lt;PROJECT_NAME&gt;</Title>
+			<br />
+			<SimpleGrid cols={2}>
+				<Button size="xl" color="yellow.6">
+					Create An Invoice
+				</Button>
+				<Button size="xl" variant="outline" color="yellow.6">
+					Search Invoices
+				</Button>
+				<Button size="xl" color="orange.6">
+					Create A Customer
+				</Button>
+				<Button size="xl" color="orange.6" variant="outline">
+					Search Customers
+				</Button>
+			</SimpleGrid>
+		</Card>
+	);
 };
 
 // This is the website's home page.
